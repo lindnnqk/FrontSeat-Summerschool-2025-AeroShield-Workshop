a4941460fa8e81e3a653e79285826f5be9109a9a
unknown <rdy@RDY-Laptop.rus.rub.de>
2025-09-03 09:01:30 +0200

    AeroShield teensy project

---
+++ AutomationShield/src/sampling/SamplingTeensy_ISR.h
@@ -0,0 +1,18 @@
+/*
+  ISR for handling the interrupt-driven sampling for 
+  real-time control on the Arduino Mega 2560.
+  
+  This code is part of the AutomationShield hardware and software
+  ecosystem. Visit http://www.automationshield.com for more
+  details. This code is licensed under a Creative Commons
+
+  Benedikt Lammersmann, 2025
+  Last update: 22.08.2025.
+*/
+
+#ifndef SAMPLINGTEENSY_ISR_H
+#define SAMPLINGTEENSY_ISR_H
+
+
+// No definitions for Teensy needed so far, added for streamlining!
+#endif
